5b20a57a8222aee85a5731e937cafd223b456b4f
Lilian DAMIENS <ldamiens@antsys.intra>
2020-10-22 11:20:37 +0200

    Ajout du convertisseur vers Modèle

---
+++ src/core/converter/ModelConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace noxORM.src.core.converter
+{
+    /// <summary>
+    /// This class is used to convert a Class into a classic model.
+    /// </summary>
+    public class ModelConverter
+    {
+        #region Fields
+
+
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor of ModelConverter.
+        /// </summary>
+        public ModelConverter()
+        {
+
+        }
+
+        #endregion
+
+        #region Methods
+
+        public string testyo()
+        {
+            return ("testyo");
+        }
+
+        #endregion
+    }
+}
