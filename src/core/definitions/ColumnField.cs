c930e0781967ab8230372661c397fbaa42562ca1
Lilian DAMIENS <ldamiens@antsys.intra>
2020-10-22 11:21:21 +0200

    Ajout des definitions, qui sont des 'modèles' pour l'orm

---
+++ src/core/definitions/ColumnField.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace noxORM.src.core.definitions
+{
+    /// <summary>
+    /// This class combines column & field.
+    /// </summary>
+    class ColumnField
+    {
+        #region Fields
+
+        /// <summary>
+        /// Program propertie name
+        /// </summary>
+        public string propertieName { get; set; }
+        /// <summary>
+        /// Database column name
+        /// </summary>
+        public string columnName { get; set; }
+        /// <summary>
+        /// Database column type
+        /// </summary>
+        public DbType columnType { get; set; }
+        /// <summary>
+        /// Database column value length
+        /// </summary>
+        public string length { get; set; }
+        /// <summary>
+        /// Is this column nullable ?
+        /// </summary>
+        public bool nullable { get; set; }
+        /// <summary>
+        /// Comment of the column
+        /// </summary>
+        public string comment { get; set; }
+
+        //todo A TESTER
+
+        public string isPrimary { get; set; }
+        public string isForeign { get; set; }
+        public string foreignPropertieName { get; set; }
+        public string foreignTableName { get; set; }
+
+        #endregion
+    }
+}
---
+++ src/core/definitions/Model.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace noxORM.src.core.definitions
+{
+    class Model
+    {
+        #region Fields
+
+        public string tableName { get; set; }
+        public Dictionary<string, ColumnField> columnProperties { get; private set; }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Main constructor of the Model class.
+        /// </summary>
+        /// <param name="tableName">Name of the database table</param>
+        /// <param name="columnProperties">Key value array which contains name of columns and their properties</param>
+        public Model(string tableName, Dictionary<string, ColumnField> columnProperties)
+        {
+            this.tableName = tableName;
+            this.columnProperties = columnProperties;
+        }
+
+        #endregion
+    }
+}
