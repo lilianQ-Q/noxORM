e7187c0f2c7891cc0b62671fd1ec8d74178175bc
Lilian DAMIENS <ldamiens@antsys.intra>
2020-10-23 17:20:23 +0200

    Ajout du type d'erreur de conversion de type

---
+++ src/core/exceptions/DatabaseTypeErrorException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace noxORM.src.core.exceptions
+{
+    /// <summary>
+    /// This class is used to create new Exceptions about .NET framwork types when the orm didn't find their sql server equivalent.
+    /// </summary>
+    public class DatabaseTypeErrorException : NoxORMException
+    {
+        #region Fields
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Main constructor of DatabaseTypeErrorException class. Use it when you are unable to find a .net framework type equivalent in sql server.
+        /// </summary>
+        /// <param name="message"></param>
+        public DatabaseTypeErrorException(string message) : base("DatabaseTypeError", message)
+        {
+
+        }
+
+        #endregion
+    }
+}
