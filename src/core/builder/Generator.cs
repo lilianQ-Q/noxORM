8f0fd364e558e4bb5eb7ff29673abcab88cee382
Lilian DAMIENS <ldamiens@antsys.intra>
2020-10-23 17:16:37 +0200

    Prémisses de l'autoconception des requêtes

---
+++ src/core/builder/Generator.cs
@@ -0,0 +1,50 @@
+﻿using noxORM.src.core.builder.interfaces;
+using noxORM.src.core.definitions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace noxORM.src.core.builder
+{
+    /// <summary>
+    /// This class is used to generate SQL queries from Nox Models.
+    /// </summary>
+    public class Generator : IGenerator
+    {
+        #region Fields
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default construcot of Generator class.
+        /// </summary>
+        public Generator()
+        {
+
+        }
+
+        #endregion
+
+        public string Insert(Model model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Select(Model model)
+        {
+            StringBuilder query = new StringBuilder();
+            query.Append(" SELECT ");
+
+            int count = 1;
+            foreach(ColumnField field in model.columnsProperties.Values)
+            {
+                query.Append(field.columnName + ", ");
+            }
+            return (query.ToString());
+        }
+    }
+}
---
+++ src/core/builder/interfaces/IGenerator.cs
@@ -0,0 +1,15 @@
+﻿using noxORM.src.core.definitions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace noxORM.src.core.builder.interfaces
+{
+    public interface IGenerator
+    {
+        string Select(Model model);
+        string Insert(Model model);
+    }
+}
