36b91390b985f58e9ef39ec318b41099df221758
Lilian DAMIENS <ldamiens@antsys.intra>
2020-10-23 10:40:42 +0200

    Ajout de la gestion d'exceptions

---
+++ src/core/exceptions/DatabaseTypeError.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace noxORM.src.core.exceptions
+{
+    public class DatabaseTypeError : NoxORMException
+    {
+        #region Fields
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Main constructor of DatabaseTypeError class. Use it when you are unable to find a .net framework type equivalent in sql server.
+        /// </summary>
+        /// <param name="message"></param>
+        public DatabaseTypeError(string message) : base("DatabaseTypeError", message)
+        {
+
+        }
+
+        #endregion
+    }
+}
---
+++ src/core/exceptions/NoxORMException.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace noxORM.src.core.exceptions
+{
+    [Serializable]
+    public class NoxORMException : Exception, ISerializable
+    {
+        #region Fields
+
+        public string errorCode { get { return this._errorCode; } } 
+        private string _errorCode { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Main constructor of noxORMException class
+        /// </summary>
+        /// <param name="errorCode"></param>
+        /// <param name="errorMessage"></param>
+        public NoxORMException(string errorCode, string errorMessage) : base(errorMessage)
+        {
+            this._errorCode = errorCode;
+        }
+
+        #endregion
+    }
+}
