06d376df554add5fcb306203e70aab060ad6faa4
Lilian DAMIENS <ldamiens@antsys.intra>
2020-10-23 10:48:18 +0200

    Gestion de la non génération de certains colonne dans la bdd

---
+++ src/core/attributes/NotGenerateColumn.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace noxORM.src.core.attributes
+{
+    /// <summary>
+    /// This class is used to tell to the orm to not generate the targeted column in the database.
+    /// </summary>
+    public class NotGenerateColumn : BaseAttribute
+    {
+        #region Fields
+
+
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Main constructor of the NotGenerateColumn class
+        /// </summary>
+        public NotGenerateColumn()
+        {
+
+        }
+
+        #endregion
+    }
+}
