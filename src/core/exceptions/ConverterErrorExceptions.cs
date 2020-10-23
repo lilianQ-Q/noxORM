8ccf26c4d859a30ef7677b65694087a1695282b6
Lilian DAMIENS <ldamiens@antsys.intra>
2020-10-23 17:18:32 +0200

    Ajout du type d'erreur ConverterError

---
+++ src/core/exceptions/ConverterErrorExceptions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace noxORM.src.core.exceptions
+{
+    /// <summary>
+    /// This class is used to create new exceptions about noxORM converter error
+    /// </summary>
+    public class ConverterErrorExceptions : NoxORMException
+    {
+        #region Fields
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Main constructor of the ConverterErrorExceptions class.
+        /// </summary>
+        /// <param name="message">Describes the error itself</param>
+        public ConverterErrorExceptions(string message) : base ("ConverterError", message)
+        {
+
+        }
+
+        #endregion
+    }
+}
