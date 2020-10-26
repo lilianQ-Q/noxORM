0ff0a04aa5329d6356c0be818ddcb837e3315a0f
Lilian DAMIENS <ldamiens@antsys.intra>
2020-10-26 09:30:48 +0100

    Ajout du convertisseur d'entitées / DataModels

---
+++ src/core/converter/EntitieConverter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace noxORM.src.core.converter
+{
+    class EntitieConverter
+    {
+        //L'inverse de modelConverter
+    }
+}
